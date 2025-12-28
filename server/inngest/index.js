783927f994faf5470b676bbd8c23882b6cde8dda
Srishti Agarwal <srishti.agarwal60@gmail.com>
2025-12-28 10:27:18 +0100

    Added server functionality and connections with Neon and Inngest

---
+++ server/inngest/index.js
@@ -0,0 +1,58 @@
+import { id } from "effect/Fiber";
+import { Inngest } from "inngest";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "project-management" });
+
+// Inngest Function to save user data to a database
+const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-from-clerk'},
+    {event: 'clerk/user.created'},
+    async ({ event }) =>{
+        const {data} = event
+        await prisma.user.create({
+            data: {
+                id: data.id,
+                email: data?.email_addresses[0]?.email_addres,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url,
+            }
+        })
+    }
+)
+
+// Inngest Function to delete user from database
+const syncUserDeletion = inngest.createFunction(
+    {id: 'delete-user-from-clerk'},
+    {event: 'clerk/user.deleted'},
+    async ({ event }) =>{
+        const {data} = event
+        await prisma.user.delete({
+            where: {
+                id: data.id,
+            }
+        })
+    }
+)
+
+// Inngest Function to update user data in database
+const syncUserUpdation = inngest.createFunction(
+    {id: 'update-user-from-clerk'},
+    {event: 'clerk/user.updated'},
+    async ({ event }) =>{
+        const {data} = event
+        await prisma.user.update({
+            where: {
+                id: data.id,
+            },
+            data: {
+                email: data?.email_addresses[0]?.email_addres,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url,
+            }
+        })
+    }
+)
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
